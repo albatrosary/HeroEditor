5de8938f9640b57e1ecd6dda20914bd08934ca0c
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-09-15 22:16:01 +0900

    add enableProdMode()

--- src/main.aot.ts
+++ src/main.aot.ts
@@ -1,3 +1,5 @@
 import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './shared/app.module.ngfactory';
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
