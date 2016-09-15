5c104ae53ee6f268ae362604729411442a6394f3
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-09-15 23:11:01 +0900

    change rxjs-extensions.ts

--- src/components/rxjs-extensions.ts
+++ src/components/rxjs-extensions.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
-
+import 'rxjs/add/operator/toPromise';
 
 /*
 Copyright 2016 Google Inc. All Rights Reserved.
